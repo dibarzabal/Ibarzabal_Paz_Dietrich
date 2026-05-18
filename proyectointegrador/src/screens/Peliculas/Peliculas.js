97e142e1c539c7c86e47cc9400099df664347d3c
guadapaz1 <pazga@udesa.edu.ar>
2026-05-17 22:11:16 -0300

    peliculas pasado a hooks

--- proyectointegrador/src/screens/Peliculas/Peliculas.js
+++ proyectointegrador/src/screens/Peliculas/Peliculas.js
@@ -1,78 +1,87 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../../components/Card/Card";
 import Loader from "../../components/Loader/Loader";
 
-class Peliculas extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      peliculas: [],
-      page: 1,
-      texto: ""
-    };
-  }
+function Peliculas() {
+   const [peliculas, setPeliculas] = useState([]);
+  const [page, setPage] = useState(1);
+  const [texto, setTexto] = useState("");
 
-  componentDidMount() {
-    this.cargarPeliculas();
-  }
+  useEffect(()=> {
+cargarPeliculas();
+  }, []);
 
-  controlarCambios(event) {
-    this.setState({
-     texto: event.target.value
-    });
-  }
-
-  filtrarPeliculas() {
-    return this.state.peliculas.filter(
-      (unaPeli) => unaPeli.title.toLowerCase().includes(this.state.texto.toLowerCase())
-    );
-  }
-
-  cargarPeliculas() {
-  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=41abfd625c63035603389ca24c10eed0&page=${this.state.page}`)
-    .then(response => response.json())
-    .then(data => {
-      let nuevas = [];
-
-      for (let i = 0; i < this.state.peliculas.length; i++) {
-        nuevas.push(this.state.peliculas[i]);
-      }
-
-      for (let i = 0; i < data.results.length; i++) {
-        nuevas.push(data.results[i]);
-      }
-
-      this.setState({
-        peliculas: nuevas
-      });
-    })
-    .catch(error => console.log("El error fue " + error));
+function controlarCambios(event) {
+    ssetTexto(event.target.value);
 }
 
-  cargarMas() {
-    this.setState(
-      {page: this.state.page + 1},
-      () => this.cargarPeliculas() 
+function filtrarPeliculas() {
+    return peliculas.filter(
+      (unaPeli) =>
+        unaPeli.title.toLowerCase().includes(texto.toLowerCase())
     );
   }
 
-  render() {
-    let peliculasFiltradas = this.filtrarPeliculas();
+  function cargarPeliculas() {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=41abfd625c63035603389ca24c10eed0&page=${page}`)
+      .then(response => response.json())
+      .then(data => {
+        let nuevas = [];
+
+        for (let i = 0; i < peliculas.length; i++) {
+          nuevas.push(peliculas[i]);
+        }
+
+        for (let i = 0; i < data.results.length; i++) {
+          nuevas.push(data.results[i]);
+        }
+
+        setPeliculas(nuevas);
+      })
+      .catch(error => console.log("El error fue " + error));
+  }
+
+  function cargarMas() {
+    let paginaNueva = page + 1;
+
+    setPage(paginaNueva);
+
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=41abfd625c63035603389ca24c10eed0&page=${paginaNueva}`)
+      .then(response => response.json())
+      .then(data => {
+        let nuevas = [];
+
+        for (let i = 0; i < peliculas.length; i++) {
+          nuevas.push(peliculas[i]);
+        }
+
+        for (let i = 0; i < data.results.length; i++) {
+          nuevas.push(data.results[i]);
+        }
+
+        setPeliculas(nuevas);
+      })
+      .catch(error => console.log("El error fue " + error));
+  }
+
+  let peliculasFiltradas = filtrarPeliculas();
+
+
     return (
       <section className="container">
         <form className="search-form">
-        <input type="text" placeholder="Buscar..." value={this.state.texto} onChange={(event) => this.controlarCambios(event)}/>
+        <input type="text" placeholder="Buscar..." value={texto} onChange={(event) => controlarCambios(event)}/>
       </form>
         <h2 className="alert alert-primary">Todas las películas</h2>
 
         <section className="row cards">
-           {this.state.peliculas.length === 0 ? (<Loader />) : 
+           {peliculas.length === 0 ? (<Loader />) : 
            (peliculasFiltradas.map((elemento, idx) => (<Card key={elemento +idx} data={elemento}/>)))}
         </section>
         <button onClick={() => this.cargarMas()} className="btn btn-primary"> Cargar más </button>
       </section>
     );
   }
-}
+
 
 export default Peliculas;
