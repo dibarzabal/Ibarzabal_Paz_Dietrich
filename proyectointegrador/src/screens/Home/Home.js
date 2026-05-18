a05c5a4299a785be4bd47d75a4fac0c6a5ebd6ad
guadapaz1 <pazga@udesa.edu.ar>
2026-05-17 21:45:28 -0300

    home pasado a hooks

--- proyectointegrador/src/screens/Home/Home.js
+++ proyectointegrador/src/screens/Home/Home.js
@@ -1,34 +1,26 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../../components/Card/Card";
 import Loader from "../../components/Loader/Loader";
 import Formulario from "../../components/Formulario/Formulario";
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      peliculas: [],
-      series: []
-    };
+function Home() {
+  const [peliculas, setPeliculas] = useState([]);
+  const [series, setSeries] = useState([])
+
+useEffect(() =>  {
+
+fetch("https://api.themoviedb.org/3/movie/popular?api_key=41abfd625c63035603389ca24c10eed0")
+      .then(response => response.json())
+            .then(data => setPeliculas(data.results))
+            .catch(error => console.log("El error fue" + error))
+
+ fetch("https://api.themoviedb.org/3/tv/popular?api_key=41abfd625c63035603389ca24c10eed0")
+      .then(response => response.json())
+            .then(data => setSeries(data.results))
+            .catch(error => console.log("El error fue" + error))
+}, [])
   }
 
-  componentDidMount() {
-    fetch("https://api.themoviedb.org/3/movie/popular?api_key=41abfd625c63035603389ca24c10eed0")
-      .then(response => response.json())
-            .then(data => this.setState(
-                {peliculas: data.results}
-            ))
-            .catch(error => console.log("El error fue" + error))
-
-    fetch("https://api.themoviedb.org/3/tv/popular?api_key=41abfd625c63035603389ca24c10eed0")
-      .then(response => response.json())
-            .then(data => this.setState(
-                {series: data.results}
-            ))
-            .catch(error => console.log("El error fue" + error))
-    }
-
-  render() {
     return (
     
       <>
@@ -37,20 +29,18 @@ class Home extends Component {
         <section className="container">
             <h2 className="alert alert-primary">Movies now playing</h2>
             <section className="row cards" id="now-playing">
-                {this.state.peliculas.length === 0 ? <Loader/> :
-                this.state.peliculas.slice(0, 6).map((elemento, idx) => <Card key={elemento +idx} data={elemento}/> )}        
+                {peliculas.length === 0 ? <Loader/> :
+                peliculas.slice(0, 6).map((elemento, idx) => <Card key={elemento +idx} data={elemento}/> )}        
             </section>
             <a href="/peliculas">Ver todas</a>
             <h2 className="alert alert-warning">Popular TV shows this week</h2>
             <section className="row cards tv-show">
-                {this.state.series.length === 0 ? <Loader/> :
-                this.state.series.slice(0, 6).map((elemento, idx) => <Card key={elemento +idx} data={elemento}/> )}
+                {series.length === 0 ? <Loader/> :
+                series.slice(0, 6).map((elemento, idx) => <Card key={elemento +idx} data={elemento}/> )}
             </section>
             <a href="/series">Ver todas</a>
         </section>
         </>
     );
-  }
-}
 
 export default Home;
